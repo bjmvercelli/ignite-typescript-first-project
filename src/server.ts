e631353f8e2ff5686ca9c0137bfadde0b084a1d2
Bruno Vercelli <bruno.vercelli@hotmail.com>
2022-05-05 23:15:27 -0300

    Add ts-node-dev flags

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => res.json({ message: 'Hello World' }))
+app.get("/", (req, res) => {
+	res.json({ message: 'Hello World' });
+})
+
+app.post("/courses", (req, res) => {
+	const { name } = req.body;
+
+	return res.json(name);
+});
 
 app.listen(3333);
