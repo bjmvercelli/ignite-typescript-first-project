91fb40674ab9603e6d3b3b0533971e42170bfd56
Bruno Vercelli <bruno.vercelli@afya.com.br>
2022-05-27 17:19:43 -0300

    Implement specifications logic + move files

---
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -0,0 +1,14 @@
+import { Specification } from "../model/Specification";
+
+
+interface ISpecificationsDTO {
+  name: string,
+  description: string;
+}
+
+interface ISpecificationsRepository {
+  create({ name, description }: ISpecificationsDTO): void;
+  findByName(name: string): Specification;
+}
+
+export { ISpecificationsRepository, ISpecificationsDTO }
