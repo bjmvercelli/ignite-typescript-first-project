a8ffbe74c41e8b8c5efa8c180c9451831758aa5f
Bruno Vercelli <bruno.vercelli@afya.com.br>
2022-08-12 18:44:52 -0300

    Add car-specification relationship**

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,6 +1,7 @@
-import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, TableForeignKey } from "typeorm";
+import { Column, CreateDateColumn, Entity, JoinColumn, JoinTable, ManyToMany, ManyToOne, PrimaryColumn, TableForeignKey } from "typeorm";
 import { v4 as uuid } from "uuid";
 import { Category } from "./Category";
+import { Specification } from "./Specification";
 
 @Entity("cars")
 class Car {
@@ -34,6 +35,22 @@ class Car {
 
   @Column()
   category_id: string;
+
+  @ManyToMany(() => Specification)
+  @JoinTable({
+    name: "specifications_cars",
+    joinColumns: [ // Nome da coluna que referencia a tabela atual
+      {
+        name: "car_id"
+      }
+    ],
+    inverseJoinColumns: [ // Nome da coluna que referencia a tabela considerada no ManyToMany
+      {
+        name: "specification_id"
+      }
+    ]
+  })
+  specifications: Specification[];
 
   @CreateDateColumn()
   created_at: Date;
