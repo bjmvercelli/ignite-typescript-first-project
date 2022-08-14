e76f3189058346b44652adb142ecc5e61e07d677
Bruno Vercelli <bruno.vercelli@afya.com.br>
2022-08-14 18:18:09 -0300

    Finish create rental logic

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,6 +5,7 @@ import { Category } from '@modules/cars/infra/typeorm/entities/Category';
 import { Specification } from '@modules/cars/infra/typeorm/entities/Specification';
 import { Car } from '@modules/cars/infra/typeorm/entities/Car';
 import { CarImage } from '@modules/cars/infra/typeorm/entities/CarImage';
+import { Rental } from '@modules/rentals/infra/typeorm/entities/Rental';
 
 const dataSource = new DataSource({
   type: 'postgres',
@@ -13,7 +14,7 @@ const dataSource = new DataSource({
   username: 'docker',
   password: 'ignite',
   database: 'rentx',
-  entities: [ Category, Specification, User, Car, CarImage ],
+  entities: [ Category, Specification, User, Car, CarImage, Rental ],
   migrations: [
     "./src/shared/infra/typeorm/migrations/*.ts"
   ]
